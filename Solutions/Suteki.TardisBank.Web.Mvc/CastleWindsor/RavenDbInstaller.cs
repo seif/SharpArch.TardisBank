479af5a7a7f068030824aa94a3b709782c363733
Seif Attar <iam@seifattar.net>
2012-08-05 17:33:10 +0100

    Configure RavenDB

---
+++ Solutions/Suteki.TardisBank.Web.Mvc/CastleWindsor/RavenDbInstaller.cs
@@ -0,0 +1,39 @@
+﻿namespace Suteki.TardisBank.Web.Mvc.CastleWindsor
+{
+    using Castle.MicroKernel;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    using Raven.Client;
+    using Raven.Client.Document;
+    using Raven.Client.Indexes;
+
+    public class RavenInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IDocumentStore>().ImplementedBy<DocumentStore>()
+                    .DependsOn(new { connectionStringName = "tardisConnection" })
+                    .OnCreate(DoInitialisation)
+                    .LifeStyle.Singleton,
+                Component.For<IDocumentSession>()
+                    .UsingFactoryMethod(GetDocumentSesssion)
+                    .LifeStyle.PerWebRequest
+                );
+        }
+
+        static IDocumentSession GetDocumentSesssion(IKernel kernel)
+        {
+            var store = kernel.Resolve<IDocumentStore>();
+            return store.OpenSession();
+        }
+
+        public static void DoInitialisation(IKernel kernel, IDocumentStore store)
+        {
+            store.Initialize();
+            //IndexCreation.CreateIndexes(typeof(Child_ByPendingSchedule).Assembly, store);
+        }
+    }
+}
